e34a92ffef8c86874c824f2f61a571d4ea6331bf
Jeff Au <jeffau13@gmail.com>
2019-11-10 19:16:44 -0500

    created initial app with webpack and babel

---
+++ app/components/Popular.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
+  render() {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul className="flex-center">
+        {languages.map(language => (
+          <li key={language}>
+            <button
+              className="btn-clear nav-link"
+              onClick={() => this.updateLanguage(language)}
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: 'blue' }
+                  : null
+              }
+            >
+              {language}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
