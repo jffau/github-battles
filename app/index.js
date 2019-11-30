deaf0e4f2924a0ae4acb899d1b53297cb386f736
Jeff Au <jeffau13@gmail.com>
2019-11-29 21:24:06 -0500

    theme provider

--- app/index.js
+++ app/index.js
@@ -4,15 +4,31 @@ var ReactDOM = require("react-dom");
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
 
+import { ThemeProvider } from "./contexts/theme";
+
 require("./index.css");
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light"
+        }));
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Popular />
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
