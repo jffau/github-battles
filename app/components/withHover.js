4e1c864565ea7a919f8a91e0ff87a39720fa7b3f
Jeff Au <jeffau13@gmail.com>
2019-12-01 16:47:26 -0500

    refactored to class fields

--- app/components/withHover.js
+++ app/components/withHover.js
@@ -2,26 +2,19 @@ import React from "react";
 
 export default function withHover(Component, propName = "hovering") {
   return class WithHover extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        hovering: false
-      };
-
-      this.mouseOver = this.mouseOver.bind(this);
-      this.mouseOut = this.mouseOut.bind(this);
-    }
-    mouseOver() {
+    state = {
+      hovering: false
+    };
+    mouseOver = () => {
       this.setState({
         hovering: true
       });
-    }
-    mouseOut() {
+    };
+    mouseOut = () => {
       this.setState({
         hovering: false
       });
-    }
+    };
     render() {
       const props = {
         [propName]: this.state.hovering,
